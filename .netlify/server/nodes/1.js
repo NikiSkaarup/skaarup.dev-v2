4208cdacbea2b8419f2a4cb2f6ace6176e432ee2
Niki Wix Skaarup <NikiSkaarup@users.noreply.github.com>
2022-09-24 16:06:11 +0200

    .netlify

--- .netlify/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-1f12c5cb.js';
-export const imports = ["_app/immutable/components/error.svelte-1f12c5cb.js","_app/immutable/chunks/index-66220184.js","_app/immutable/chunks/stores-10692b37.js","_app/immutable/chunks/singletons-a9f8393e.js","_app/immutable/chunks/index-e366d1ab.js"];
-export const stylesheets = [];
