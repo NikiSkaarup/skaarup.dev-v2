1e516433fdb5370971a5569e140b2d7ed89ec68a
Niki Wix Skaarup <niki@skaarup.dev>
2023-06-23 22:22:03 +0200

    actually clean slate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
 	import '../app.postcss';
 </script>
 
 <svelte:head>
 	<title>Niki Wix Skaarup</title>
 </svelte:head>
-
-<Header />
 
 <main class="container mx-auto grow">
 	<slot />
