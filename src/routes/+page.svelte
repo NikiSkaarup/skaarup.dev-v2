c81feae3acfa19211956e01e611adcb3db08c61b
Niki Wix Skaarup <niki@skaarup.dev>
2023-07-06 23:04:12 +0200

    add incomplete uses page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,11 @@
 	export let data;
 </script>
 
-{#each data.posts as post, i}
-	<article class:bg-black={i % 2 !== 0}>
+{#each data.posts as post (post.slug)}
+	<article>
 		<header class="container mx-auto w-full space-y-4 px-4 py-8">
 			<h1 class="hyphens-auto break-words text-center text-3xl">{post.title}</h1>
-			<p class="mx-auto max-w-prose hyphens-auto break-words">
+			<p class="mx-auto max-w-prose hyphens-auto break-words text-center">
 				{post.snippet}
 			</p>
 			<div class="mx-auto max-w-prose">
@@ -22,3 +22,9 @@
 {:else}
 	<p class="text-center py-8">No posts yet.</p>
 {/each}
+
+<style lang="postcss">
+	article:nth-of-type(even) {
+		@apply bg-black;
+	}
+</style>
