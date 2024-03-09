e9d8ecbf1b952fbb65ab96943f666a709760e5e6
Niki Wix Skaarup <niki@skaarup.dev>
2024-03-09 21:01:28 +0100

    performance monitoring

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+declare global {
+	declare namespace App {
+		interface Locals {
+			requestId: string;
+		}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
+	}
+
+	var performanceObserver: PerformanceObserver;
 }
 
 export {};
--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,62 @@
+import { dev } from '$app/environment';
+import { nanoid } from 'nanoid';
+
 export const handle = async ({ event, resolve }) => {
-	return resolve(event);
+	const requestId = nanoid();
+	const startMark = `request/start/${requestId}`;
+	performance.mark(startMark);
+
+	event.locals.requestId = requestId;
+
+	const result = await resolve(event);
+
+	performance.measure(`request: ${requestId}`, startMark);
+	return result;
 };
+
+// export async function handleFetch({ request, fetch }) {
+// 	return fetch(request);
+// }
+
+export async function handleError({ error, event, status, message }) {
+	console.error({
+		message,
+		path: event.url.pathname,
+		query: event.url.search,
+		status,
+		error: error instanceof Error ? error.message : error
+	});
+
+	if (error instanceof Error) {
+		console.error(error);
+	}
+
+	return {
+		message: 'Whoops!'
+	};
+}
+
+if (dev) {
+	/**
+	 * @param {PerformanceObserverEntryList} entries
+	 * @param {PerformanceObserver} observer
+	 */
+	function observerCallback(entries, observer) {
+		const entriesArr = [...entries.getEntries()];
+
+		for (let i = entriesArr.length; i--; ) {
+			const entry = entriesArr[i];
+			if (entry.entryType === 'measure') {
+				console.log(`${entry.name}: ${entry.duration.toFixed(3)}ms`);
+			}
+		}
+	}
+
+	if (globalThis.performanceObserver) {
+		globalThis.performanceObserver.disconnect();
+	}
+
+	const observer = new PerformanceObserver(observerCallback);
+	observer.observe({ entryTypes: ['measure'] });
+	globalThis.performanceObserver = observer;
+}
