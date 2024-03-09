06c7a737c3570d3457635f5f0a5e7ef749e0edb6
Niki Wix Skaarup <niki@skaarup.dev>
2024-03-09 21:23:50 +0100

    formatting

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,12 +4,9 @@ import colors from '$lib/server/colors';
 
 export const handle = async ({ event, resolve }) => {
 	const requestId = nanoid();
-	performance.mark(requestId);
-
 	event.locals.requestId = requestId;
-
+	performance.mark(requestId);
 	const result = await resolve(event);
-
 	performance.measure(`request ${requestId}`, requestId);
 	return result;
 };
@@ -37,18 +34,14 @@ export async function handleError({ error, event, status, message }) {
 }
 
 if (dev) {
-	/**
-	 * @param {PerformanceObserverEntryList} entries
-	 * @param {PerformanceObserver} observer
-	 */
-	function observerCallback(entries, observer) {
+	/** @type {PerformanceObserverCallback} */
+	function observerCallback(entries) {
 		const entriesArr = [...entries.getEntries()];
-
 		for (let i = entriesArr.length; i--; ) {
 			const entry = entriesArr[i];
 			if (entry.entryType === 'measure') {
-				console.log(
-					`${colors.fgGreen}${entry.name}${colors.reset} ${colors.fgYellow}${entry.duration.toFixed(3)}${colors.reset}ms`
+				console.info(
+					`${colors.fgGreen}${entry.name}${colors.reset} ${colors.fgYellow}${entry.duration.toFixed(3).padStart(7, '0')}${colors.reset}ms`
 				);
 			}
 		}
