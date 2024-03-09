5eb798f406eb13b521a988eaba83e030215a0744
Niki Wix Skaarup <niki@skaarup.dev>
2024-03-09 17:06:59 +0100

    add github logo

---
+++ src/lib/components/ui/github-logo.svelte
@@ -0,0 +1,18 @@
+<script>
+	import GithubLogoWhite from '$lib/../images/github-mark-white.svg';
+	import GithubLogoDark from '$lib/../images/github-mark.svg';
+
+	export let dark = false;
+
+	/** @type {string}*/
+	let myClass = '';
+
+	export { myClass as class };
+</script>
+
+<img
+	class="aspect-square {myClass}"
+	src={dark ? GithubLogoDark : GithubLogoWhite}
+	loading="eager"
+	alt="Github logo"
+/>
