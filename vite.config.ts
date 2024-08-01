5db5c189a8f9be55b2e9ff664401c43b17c85d3c
Niki Wix Skaarup <niki@skaarup.dev>
2024-08-01 20:03:35 +0200

    updated deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteInspector } from '@sveltejs/vite-plugin-svelte-inspector';
 import tailwindcss from '@tailwindcss/vite';
+import { ConsolePlugin } from 'sveltekit-plugin-console';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), svelteInspector()],
+	plugins: [tailwindcss(), sveltekit(), svelteInspector(), ConsolePlugin()],
 });
